3c981f206d17eb3b841961ccdd8982b8fb685666
rohithrajasekharan <rohithrajasekharan@outlook.com>
2024-01-25 14:35:17 +0530

    Spotlight in home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,26 @@ import Projects from "@/components/home/Projects";
 import Client from "@/components/home/Client";
 import Contact from "@/components/home/Contact";
 import News from "@/components/home/News";
+import { createClient } from "contentful";
 
-export default function Home() {
+
+async function retrieveData(){
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  });
+
+  try {
+    const response = await client.getEntries();
+    return response.items;
+  } catch (error) {
+    console.error("Error fetching data from Contentful:", error);
+    return [];
+  }
+}
+
+export default async function Home() {
+  const articles = await retrieveData();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <Banner />
@@ -17,7 +35,7 @@ export default function Home() {
       <Projects />
       <Client />
       <Contact />
-      <News />
+      <News data={articles}/>
     </main>
   );
 }
--- components/home/News.tsx
+++ components/home/News.tsx
@@ -1,7 +1,17 @@
-import { Button } from "../ui/button";
+import Link from "next/link";
 import Image from "next/image";
 
-export default function News() {
+function getMonthName(monthIndex: any) {
+  if (/^(0[1-9]|1[0-2])$/.test(monthIndex)) {
+    const date = new Date(`2022-${monthIndex}-01`);
+    const options: any = { month: "long" };
+    return date.toLocaleDateString("en-US", options);
+  } else {
+    return "Invalid month index";
+  }
+}
+
+export default function News(props:any) {
   return (
     <div className="bg-white py-12 px-8">
       <div className="w-fit items-center justify-center px-3 py-1 text-sm text-black bg-gray-200 border-l-4 border-red-500 mb-4 mx-auto">
@@ -9,113 +19,48 @@ export default function News() {
       </div>
       <h2 className="text-2xl font-bold mb-12 text-center">Our Latest News</h2>
       <div className="grid gap-4 justify-center">
-        <div className="grid grid-cols-1 md:grid-cols-7 justify-start items-start mx-auto max-w-4xl">
-          <div className="col-span-3 justify-center items-center mb-4">
-            <Image
-              alt="News image"
-              className="w-full h-50 object-cover object-top"
-              height={500}
-              src="/blog1.png"
-              style={{
-                aspectRatio: "200/150",
-                objectFit: "cover",
-              }}
-              width={700}
-            />
-          </div>
-          <div className="col-span-1 justify-center items-start">
-            <div className="flex flex-col md:items-center mb-4">
-              <span className="text-lg font-semibold">08</span>
-              <span className="text-sm">September</span>
+        {props.data.map((item: any, index: any) => (
+          <div
+            key={index}
+            className="grid grid-cols-1 md:grid-cols-7 justify-start items-start mx-auto max-w-4xl"
+          >
+            <div className="col-span-3 justify-center items-center mb-4">
+              <Image
+                alt="News image"
+                className="w-full h-50 object-cover object-top"
+                height={500}
+                src={`https:${item.fields.featureImage.fields.file.url}`}
+                style={{
+                  aspectRatio: "200/150",
+                  objectFit: "cover",
+                }}
+                width={700}
+              />
+            </div>
+            <div className="col-span-1 justify-center items-start">
+              <div className="flex flex-col md:items-center mb-4">
+                <span className="text-lg font-semibold">
+                  {item.fields.publishedOn.split("-")[2]}
+                </span>
+                <span className="text-sm">
+                  {getMonthName(item.fields.publishedOn.split("-")[1])}
+                </span>
+              </div>
+            </div>
+            <div className="col-span-3 mb-8">
+              <Link href={`/spotlight/${item.fields.slug}`}><h3 className="text-xl font-semibold mb-2">
+                {item.fields.title}
+              </h3></Link>
+              <p className="text-sm mb-4 whitespace-pre-wrap">
+                {item.fields.description}
+              </p>
             </div>
           </div>
-          <div className="col-span-3 mb-8">
-            <h3 className="text-xl font-semibold mb-2">
-              Inland freight a worthy solution for your business
-            </h3>
-            <p className="text-sm mb-4">
-              We are dedicated in creating added value for our customers by
-              implementing modern technology in our services.
-            </p>
-            <ul className="text-sm list-disc pl-5">
-              <li>Urgent transportation</li>
-              <li>Digital & automated solutions</li>
-              <li>Reliable & experienced staff</li>
-            </ul>
-          </div>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-7 justify-start items-start mx-auto max-w-4xl">
-          <div className="col-span-3 justify-center items-center mb-4">
-            <Image
-              alt="News image"
-              className="w-full h-50 object-cover object-top"
-              height={500}
-              src="/blog2.png"
-              style={{
-                aspectRatio: "200/150",
-                objectFit: "cover",
-              }}
-              width={700}
-            />
-          </div>
-          <div className="col-span-1 justify-center items-start">
-            <div className="flex flex-col md:items-center mb-4">
-              <span className="text-lg font-semibold">12</span>
-              <span className="text-sm">September</span>
-            </div>
-          </div>
-          <div className="col-span-3 mb-8">
-            <h3 className="text-xl font-semibold mb-2">
-              How technology can help redraw the supply chain map
-            </h3>
-            <p className="text-sm mb-4">
-              We are dedicated in creating added value for our customers by
-              implementing modern technology in our services.
-            </p>
-            <ul className="text-sm list-disc pl-5">
-              <li>Urgent transportation</li>
-              <li>Digital & automated solutions</li>
-              <li>Reliable & experienced staff</li>
-            </ul>
-          </div>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-7 justify-start items-start mx-auto max-w-4xl">
-          <div className="col-span-3 justify-center items-center mb-4">
-            <Image
-              alt="News image"
-              className="w-full h-50 object-cover object-top"
-              height={500}
-              src="/blog3.png"
-              style={{
-                aspectRatio: "200/150",
-                objectFit: "cover",
-              }}
-              width={700}
-            />
-          </div>
-          <div className="col-span-1 justify-center items-start">
-            <div className="flex flex-col md:items-center mb-4">
-              <span className="text-lg font-semibold">25</span>
-              <span className="text-sm">September</span>
-            </div>
-          </div>
-          <div className="col-span-3 mb-8">
-            <h3 className="text-xl font-semibold mb-2">
-              Five things you should have ready for your broker
-            </h3>
-            <p className="text-sm mb-4">
-              We are dedicated in creating added value for our customers by
-              implementing modern technology in our services.
-            </p>
-            <ul className="text-sm list-disc pl-5">
-              <li>Urgent transportation</li>
-              <li>Digital & automated solutions</li>
-              <li>Reliable & experienced staff</li>
-            </ul>
-          </div>
-        </div>
+        ))}
       </div>
-      <Button className="text-sm lg:text-base bg-[#2B2B2B] text-white py-2 rounded my-[35px] mb-12 block mx-auto min-w-[150px]">More Blog</Button>
+      {/* <Button className="text-sm lg:text-base bg-[#2B2B2B] text-white py-2 rounded my-[35px] mb-12 block mx-auto min-w-[150px]">
+        More Blog
+      </Button> */}
     </div>
   );
 }
